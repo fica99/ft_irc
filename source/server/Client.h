18c7d39c6bce19c14150c6e5cbd7e6af1f090365
Many Marti <arkaev.m98@gmail.com>
2022-05-08 19:03:27 -0400

    stole send functions

--- source/server/Client.h
+++ source/server/Client.h
@@ -8,6 +8,7 @@ public:
     std::string obuf;
     std::string inbuf;
     std::string nickname;
+    int errcode;
 private:
     bool ready_for_flush;
     Client operator=(const Client& lhs);
--- source/server/Server.h
+++ source/server/Server.h
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #include <iostream>
 #include "server/Client.h"
+#include "server/commands/commands/answers.h"
 #define MAX_CONNECTIONS 1024
 #define LISTEN_QUEUE 128
 #define RECV_BUF 513
@@ -25,6 +26,8 @@ private:
     std::vector<struct pollfd> userpfd;
     std::map<int, Client> clients;
     Client *curr;
+    int	sendError(int fd, int err, const std::vector<std::string> args);
+    int		sendReply(int fd, int rpl, const std::vector<std::string> &args);
 public:
     void recv_from_client();
     void accept_conn();
