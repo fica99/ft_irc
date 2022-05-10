63dcd5a94bb712d720870c940bbbbc9a38e16b3b
Filip Crnobrnja <fica.c@yandex.ru>
2022-05-10 15:17:29 +0300

    [IRCCommands] Add ValidateArgs method to KILL command

--- source/server/commands/commands/irckillcommand.cpp
+++ source/server/commands/commands/irckillcommand.cpp
@@ -3,6 +3,8 @@
 #include "server/commands/commands/irckillcommand.h"
 
 #include "server/commands/commands/irccommands.h"
+#include "server/commands/parsing/ircparsinghelper.h"
+#include "server/commands/responses/ircresponseerr_needmoreparams.h"
 #include "server/commands/responses/ircresponsesfactory.h"
 
 namespace ircserv
@@ -37,10 +39,27 @@ bool IRCKillCommand::ProcessCommand(/*serverclass */)
 
 bool IRCKillCommand::ValidateArgs(/*serverclass */)
 {
-    if (m_Args.empty())
+    if (m_Args.size() < 2)
     {
-
+        IRCResponseERR_NEEDMOREPARAMS* response = dynamic_cast<IRCResponseERR_NEEDMOREPARAMS*>(
+            GetIRCResponsesFactory().CreateResponse(Enum_IRCResponses_ERR_NEEDMOREPARAMS)
+        );
+        if (response != NULL)
+        {
+            response->SetCommand(EnumString<Enum_IRCCommands>::From(GetCommandEnum()));
+        }
+        // send response
+        GetIRCResponsesFactory().DestroyResponse(response);
         return false;
+    }
+    else
+    {
+        if (!IRCParsingHelper::IsNick(m_Args[0]))
+        {
+            return false;
+        }
+        SetNickname(m_Args[0]);
+        SetComment(m_Args[1]);
     }
     return true;
 }
