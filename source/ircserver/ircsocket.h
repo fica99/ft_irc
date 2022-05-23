8e399f258e3845b56970c74634e57042584b2ed7
Filip Crnobrnja <fica.c@yandex.ru>
2022-05-23 18:11:05 +0200

    Add non-blocking reading

--- source/ircserver/ircsocket.h
+++ source/ircserver/ircsocket.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -12,6 +13,8 @@ class IRCSocket
 {
 public:
     IRCSocket();
+    IRCSocket(uint16_t port, const std::string address, int sockFd);
+
     virtual ~IRCSocket();
 private:
     void Initialize(void);
@@ -20,6 +23,8 @@ private:
 public:
     bool CreateListenSocket(uint16_t port);
     void CloseSocket(void);
+    IRCSocket *Accept(void);
+    static int Select(std::vector<IRCSocket*>& reads, int delaySec);
 
 private:
     int Getaddrinfo(const char *port, struct addrinfo** servInfo);
@@ -27,10 +32,27 @@ private:
     int Socket(int domain, int type, int protocol);
     int Setsockopt(int level, int optname, void* optval);
     int Bind(const struct sockaddr *address, socklen_t addressLen);
+   
+
+public:
+    inline int GetSockFd(void) const { return m_SockFd; }
+    inline bool GetIsSockOpened(void) const { return m_IsSockOpened; }
+    inline uint16_t GetPort(void) { return m_Port; }
+    inline const std::string& GetAddress(void) const { return m_Address; }
+
+private:
+    inline void SetSockFd(int sockFd) { m_SockFd = sockFd; }
+    inline void SetIsSockOpened(bool isOpened) { m_IsSockOpened = isOpened; }
+    inline void SetPort(uint16_t port) { m_Port = port; }
+    inline void SetAddress(const std::string& address) { m_Address = address; }
 
 private:
     struct addrinfo m_AddressInfo;
     int m_SockFd;
+    bool m_IsSockOpened;
+    uint16_t m_Port;
+    std::string m_Address;
+
 };
 
 }
