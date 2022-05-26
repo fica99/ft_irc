c8d745cf0a42ac32a4dfd052d984b8b246a52896
Filip Crnobrnja <fica.c@yandex.ru>
2022-05-26 22:44:28 +0200

    [IRCCommands] Add NICK command

--- source/ircclient/ircclient.h
+++ source/ircclient/ircclient.h
@@ -15,14 +15,21 @@ private:
     void Shutdown(void);
 
 public:
-    inline void SetIsRegistered(bool isRegistered) { m_IsRegistered = isRegistered; };
-    inline bool GetIsRegistered(void) const { return m_IsRegistered; }
+    inline bool GetIsRegistered(void) const { return !GetNickname().empty() && !GetUsername().empty() && !GetRealname().empty(); }
     inline void SetPassword(const std::string& password) { m_Password = password; }
-    inline const std::string& GetPassword(void) { return m_Password; }
+    inline const std::string& GetPassword(void) const { return m_Password; }
+    inline void SetNickname(const std::string& nickname) { m_Nickname = nickname; }
+    inline const std::string& GetNickname(void) const { return m_Nickname; }
+    inline void SetUsername(const std::string& username) { m_Username = username; }
+    inline const std::string& GetUsername(void) const { return m_Username; }
+    inline void SetRealname(const std::string& realname) { m_Realname = realname; }
+    inline const std::string& GetRealname(void) const { return m_Realname; }
 
 private:
-    bool m_IsRegistered;
     std::string m_Password;
+    std::string m_Nickname;
+    std::string m_Username;
+    std::string m_Realname;
 };
 
 
--- source/managers/ircclientsmanager.cpp
+++ source/managers/ircclientsmanager.cpp
@@ -51,18 +51,8 @@ void IRCClientsManager::Quit(IRCSocket *socket, const std::string& quitMessage)
 
 bool IRCClientsManager::Pass(IRCSocket *socket, const std::string& password)
 {
-    std::unordered_map<IRCSocket*, IRCClient*>::iterator it = m_SocketClientsMap.find(socket);
-    IRCClient *client;
+    IRCClient *client = FindOrCreateClient(socket);
 
-    if (it != m_SocketClientsMap.end())
-    {
-        client = it->second;
-    }
-    else
-    {
-        client = New(IRCClient);
-        m_SocketClientsMap[socket] = client;
-    }
     if (client->GetIsRegistered())
     {
         return false;
@@ -74,5 +64,52 @@ bool IRCClientsManager::Pass(IRCSocket *socket, const std::string& password)
     return true;
 }
 
+Enum_IRCResponses IRCClientsManager::Nick(IRCSocket *socket, const std::string& nickname)
+{
+    if (FindClientByNickname(nickname) != NULL)
+    {
+        return Enum_IRCResponses_ERR_NICKNAMEINUSE;
+    }
+    IRCClient *client = FindOrCreateClient(socket);
+    client->SetNickname(nickname);
+    if (client->GetIsRegistered() == true)
+    {
+        if (client->GetPassword() != GetIRCServer().GetPassword())
+        {
+            Quit(socket, "");
+        }
+    }
+    return Enum_IRCResponses_Unknown;
+}
+
+IRCClient *IRCClientsManager::FindOrCreateClient(IRCSocket *socket)
+{
+    IRCClient *client;
+    std::unordered_map<IRCSocket*, IRCClient*>::iterator it = m_SocketClientsMap.find(socket);
+    
+    if (it != m_SocketClientsMap.end())
+    {
+        client = it->second;
+    }
+    else
+    {
+        client = New(IRCClient);
+        m_SocketClientsMap[socket] = client;
+        IRC_LOGD("%s", "Client created");
+    }
+    return client;
+}
+
+IRCClient *IRCClientsManager::FindClientByNickname(const std::string& nickname)
+{
+    for ( std::unordered_map<IRCSocket*, IRCClient*>::iterator it = m_SocketClientsMap.begin(); it != m_SocketClientsMap.end(); ++it)
+    {
+        if (it->second->GetNickname() == nickname)
+        {
+            return it->second;
+        }
+    }
+    return NULL;
+}
 
 }
