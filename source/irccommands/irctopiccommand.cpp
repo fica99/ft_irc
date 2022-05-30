c9a2d5601bc66aeca255ab971e1d4c828355dd0d
Filip Crnobrnja <fica.c@yandex.ru>
2022-05-30 22:18:30 +0200

    [IRCCommands] Add topic command

--- source/irccommands/irctopiccommand.cpp
+++ source/irccommands/irctopiccommand.cpp
@@ -3,6 +3,7 @@
 #include "irccommands/irctopiccommand.h"
 
 #include "irccommands/irccommands.h"
+#include "irccommands/irccommandshelper.h"
 #include "ircresponses/ircresponseshelper.h"
 #include "ircresponses/ircresponses.h"
 #include "managers/ircclientsmanager.h"
@@ -32,45 +33,56 @@ void IRCTopicCommand::Shutdown(void)
 
 bool IRCTopicCommand::ProcessCommand(IRCSocket *socket)
 {
-    // if (!GetIRCClientsManager().IsRegistered(socket))
-    // {
-    //     IRCResponsesHelper::SendResponseWithoutParams(socket, Enum_IRCResponses_ERR_NOTREGISTERED);
-    //     return false;
-    // }
+    if (!IRCCommandsHelper::IsRegistered(socket))
+    {
+        IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_NOTREGISTERED);
+        return false;
+    }
 
-    // if (ValidateArgs(socket))
-    // {
-    //     Enum_IRCResponses responseEnum = GetIRCChannelsManager().Topic(socket, GetChannel(), GetTopic());
-    //     if (responseEnum == Enum_IRCResponses_RPL_TOPIC)
-    //     {
-    //         IRCChannel *channel = GetIRCChannelsManager().FindChannel(GetChannel());
-    //         IRCResponsesHelper::SendResponseWithTopic(socket, GetChannel(), channel ? channel->GetTopic() : "");
-    //     }
-    //     else if (responseEnum == Enum_IRCResponses_ERR_NOTONCHANNEL)
-    //     {
-    //         IRCResponsesHelper::SendResponseWithServerName(socket, responseEnum, GetChannel());
-    //     }
-    //     return true;
-    // }
+
+    if (ValidateArgs(socket))
+    {
+        IRCChannel *channel = GetIRCChannelsManager().FindChannel(GetChannel());
+        if (!channel)
+        {
+            IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_NOTONCHANNEL, GetChannel());
+            return false;
+        }
+        if (!GetTopic().empty())
+        {
+            if (channel->GetModes() & TOPICSET)
+            {
+                IRCClient *client = GetIRCClientsManager().FindClient(socket);
+                if (channel->GetOpers().find(client) == channel->GetOpers().end())
+                {
+                    IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_CHANOPRIVSNEEDED, GetChannel());
+                    return false;
+                }
+            }
+            channel->SetTopic(GetTopic());
+        }
+        IRCResponsesHelper::SendTopic(socket, GetChannel(), GetTopic());
+        return true;
+    }
     return false;
 }
 
 bool IRCTopicCommand::ValidateArgs(IRCSocket *socket)
 {
-    // std::vector<std::string> channels;
-    // if (GetArgs().empty())
-    // {
-    //     IRCResponsesHelper::SendResponseWithoutParams(socket, Enum_IRCResponses_ERR_NEEDMOREPARAMS);
-    //     return false;
-    // }
-    // else
-    // {
-    //     SetChannel(GetArgs()[0]);
-    //     if (GetArgs().size() > 1)
-    //     {
-    //         SetTopic(GetArgs()[1]);
-    //     }
-    // }
+    std::vector<std::string> channels;
+    if (GetArgs().empty())
+    {
+        IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_NEEDMOREPARAMS, EnumString<Enum_IRCCommands>::From(GetCommandEnum()));
+        return false;
+    }
+    else
+    {
+        SetChannel(GetArgs()[0]);
+        if (GetArgs().size() > 1)
+        {
+            SetTopic(GetArgs()[1]);
+        }
+    }
     return true;
 }
 
