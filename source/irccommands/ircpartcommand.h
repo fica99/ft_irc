6206a4deb8e397804619934c342a43839c103b49
Filip Crnobrnja <fica.c@yandex.ru>
2022-06-01 19:42:57 +0200

    [IRCCommands] Implement PART command

--- source/irccommands/ircpartcommand.h
+++ source/irccommands/ircpartcommand.h
@@ -25,6 +25,7 @@ private:
 
 private:
     inline void SetChannels(const std::vector<std::string>& channels) { m_Channels = channels; }
+    inline const std::vector<std::string>& GetChannels(void) const { return m_Channels; }
 
 private:
     std::vector<std::string> m_Channels;
