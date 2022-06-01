2cbe74ce16a4d6b7697ffa0b9488e7e646df76ca
Filip Crnobrnja <fica.c@yandex.ru>
2022-06-01 19:11:30 +0200

    [IRCCommands] Add KICK command

--- source/irccommands/irckickcommand.cpp
+++ source/irccommands/irckickcommand.cpp
@@ -1,11 +1,15 @@
 #include "main/precomp.h"
 
 #include "irccommands/irckickcommand.h"
-#include "irccommands/irccommands.h"
 
+#include "ircclient/ircclient.h"
+#include "ircchannel/ircchannel.h"
+#include "irccommands/irccommands.h"
+#include "irccommands/irccommandshelper.h"
+#include "ircresponses/ircresponseshelper.h"
 #include "parsing/ircparsinghelper.h"
-#include "ircresponses/ircresponseerr_needmoreparams.h"
-#include "ircresponses/ircresponsesfactory.h"
+#include "managers/ircchannelsmanager.h"
+#include "managers/ircclientsmanager.h"
 
 namespace ircserv
 {
@@ -30,8 +34,37 @@ void IRCKickCommand::Shutdown(void)
 
 bool IRCKickCommand::ProcessCommand(IRCSocket *socket)
 {
+    if (!IRCCommandsHelper::IsRegistered(socket))
+    {
+        IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_NOTREGISTERED);
+        return false;
+    }
+
     if (ValidateArgs(socket))
     {
+        IRCClient *client = GetIRCClientsManager().FindClient(socket);
+        IRCChannel *channel = GetIRCChannelsManager().FindChannel(GetChannel());
+        if (!channel || !IRCCommandsHelper::IsInContainer(channel->GetClients(), client))
+        {
+            IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_NOTONCHANNEL, GetChannel());
+            return false;
+        }
+   
+        if (!IRCCommandsHelper::IsInContainer(channel->GetOpers(), client))
+        {
+            IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_CHANOPRIVSNEEDED, channel->GetName());
+            return false;
+        }
+        
+        std::string message = ":" + client->GetNickname() + "!" + client->GetUsername() + "@" + client->GetHostname() + " " + EnumString<Enum_IRCCommands>::From(GetCommandEnum()) + " " +  GetChannel() + " " + GetUser() + " :" + GetComment() + "\n";
+        if (IRCCommandsHelper::IsClientVisible(client))
+        {
+            IRCResponsesHelper::SendMessageToAllChannelNames(GetChannel(), message);
+        }
+
+        channel->RemoveClient(client);
+        channel->RemoveOper(client);
+
         return true;
     }
     return false;
@@ -39,33 +72,25 @@ bool IRCKickCommand::ProcessCommand(IRCSocket *socket)
 
 bool IRCKickCommand::ValidateArgs(IRCSocket *socket)
 {
-    // if (m_Args.size() < 2)
-    // {
-    //     IRCResponseERR_NEEDMOREPARAMS* response = dynamic_cast<IRCResponseERR_NEEDMOREPARAMS*>(
-    //         GetIRCResponsesFactory().CreateResponse(Enum_IRCResponses_ERR_NEEDMOREPARAMS)
-    //     );
-    //     if (response != NULL)
-    //     {
-    //         response->SetCommand(EnumString<Enum_IRCCommands>::From(GetCommandEnum()));
-    //     }
-    //     // server->send(response->GetResponse())
-    //     // send response
-    //     GetIRCResponsesFactory().DestroyResponse(response);
-    //     return false;
-    // }
-    // else
-    // {
-    //     if (!IRCParsingHelper::IsChannel(m_Args[0]) || !IRCParsingHelper::IsUser(m_Args[1]))
-    //     {
-    //         return false;
-    //     }
-    //     SetChannel(m_Args[0]);
-    //     SetUser(m_Args[1]);
-    //     if (m_Args.size() > 2)
-    //     {
-    //         SetComment(m_Args[2]);
-    //     }
-    // }
+    if (GetArgs().size() < 2)
+    {
+        IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_NEEDMOREPARAMS, EnumString<Enum_IRCCommands>::From(GetCommandEnum()));
+        return false;
+    }
+    else
+    {
+        if (!IRCParsingHelper::IsChannel(GetArgs()[0]))
+        {
+            IRCResponsesHelper::SendResponseWithParams(socket, Enum_IRCResponses_ERR_NOSUCHCHANNEL, GetArgs()[0]);
+            return false;
+        }
+        SetChannel(GetArgs()[0]);
+        SetUser(GetArgs()[1]);
+        if (GetArgs().size() > 2)
+        {
+            SetComment(GetArgs()[2]);
+        }
+    }
     return true;
 }
 }
--- source/irccommands/irckickcommand.h
+++ source/irccommands/irckickcommand.h
@@ -24,8 +24,11 @@ private:
 
 private:
     inline void SetChannel(const std::string& channel) { m_Channel = channel; }
+    inline const std::string& GetChannel(void) const { return m_Channel; }
     inline void SetUser(const std::string& user) { m_User = user; }
+    inline const std::string& GetUser(void) const { return m_User; }
     inline void SetComment(const std::string& comment) { m_Comment = comment; }
+    inline const std::string& GetComment(void) const { return m_Comment; }
 
 private:
     std::string m_Channel;
