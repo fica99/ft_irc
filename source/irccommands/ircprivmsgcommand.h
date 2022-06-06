75d28d7be24fc456786949e54ab9499d0dbd38a7
Filip Crnobrnja <fica.c@yandex.ru>
2022-06-06 16:20:37 +0200

    [IRCCommands] Finish PRIVMSG

--- source/irccommands/ircprivmsgcommand.h
+++ source/irccommands/ircprivmsgcommand.h
@@ -24,12 +24,10 @@ private:
     bool ValidateArgs(IRCSocket *socket);
 
 private:
-    void SendERR_NORECIPIENT(/*serverclass */);
-    void SendERR_NOTEXTTOSEND(/*serverclass */);
-
-private:
     inline void SetReceivers(const std::vector<std::string>& receivers) { m_Receivers = receivers; }
+    inline const std::vector<std::string>& GetReceivers(void) const { return m_Receivers; }
     inline void SetTextToBeSent(const std::string& textToBeSent) { m_TextToBeSent = textToBeSent; }
+    inline const std::string& GetTextToBeSent(void) const { return m_TextToBeSent; }
 
 private:
     std::vector<std::string> m_Receivers;
